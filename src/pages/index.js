e5f267fa1a763ea1f7fee504e0e155ad4aa4c460
Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-03-29 02:21:44 -0300

    Commit Initial

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+  return (
+    <header>
+      <h1>Hello</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto delectus aliquid alias incidunt obcaecati maxime odio in facere nam.</p>
+    </header>
+  )
+}
+
+export default Index
