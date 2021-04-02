cd81c82837e38e9a2634e84ec8112326809ea5f5
Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-04-02 04:24:11 -0300

    Implementação de SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
+import Head from 'next/head'
 import Hero from '../components/Hero'
 import ShapeDivider from '../components/ShapeDivider'
 
 const Index = () => {
   return (
-    <Hero>
-      <ShapeDivider/>
-    </Hero>
+    <>
+      <Head>
+        <title>Consultoria de Marketing Digital para Empreendedoras | Dayane Silva</title>
+        <meta name="description" content="Você tem uma marca no Instagram, está iniciando um novo projeto, acredita que pode crescer e mesmo assim, ultimamente não encontra uma melhor forma de se..."></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Hero>
+        <ShapeDivider/>
+      </Hero>
+    </>
   )
 }
 
