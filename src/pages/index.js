4c900f3dee3879626dfe3003cc0c41e0c559743b
Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-04-02 13:50:26 -0300

    Inserido imagem destaque e headline no Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Container from '../components/Container'
 import Hero from '../components/Hero'
 import ShapeDivider from '../components/ShapeDivider'
 
@@ -11,6 +12,15 @@ const Index = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero>
+        <Container>
+          <figure>
+            <img src="dayane-silva-consultoria-de-marketing-digital.png" alt="Imagem que ilustra a consultora Dayane Silva"/>
+          </figure>
+          <div>
+            <h1>Você acredita que pode construir uma marca incrível, mas se sente perdida no processo das estratégias?</h1>
+            <h2>Descubra como uma consultoria direcionada para o seu negócio, pode ajudar a sua marca a se destacar!</h2>
+          </div>
+        </Container>
         <ShapeDivider/>
       </Hero>
     </>
