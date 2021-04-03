a4a4eebc06a16b0266bfade3e7dedc9ef450b9de
Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-04-02 22:21:54 -0300

    Inserido animação no Hero com Framer Motion e adicionado CTA

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,30 @@
 import Head from 'next/head'
+import { motion } from 'framer-motion'
 import Container from '../components/Container'
 import Hero from '../components/Hero'
+import CTA from '../components/CTA'
 import ShapeDivider from '../components/ShapeDivider'
 
 import styles from '../styles/Home.module.scss'
+
+let easing = [0.6, -0.05, 0.01, 0.99];
+
+// Custom variant
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.6, ease: easing }
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing
+    }
+  }
+};
 
 const Index = () => {
   return (
@@ -16,11 +37,25 @@ const Index = () => {
       <Hero>
         <Container>
           <figure className={styles.featuredImage}>
-            <img src="dayane-silva-consultoria-de-marketing-digital.png" alt="Imagem que ilustra a consultora Dayane Silva"/>
+            <motion.img 
+              src="/dayane-silva-consultoria-de-marketing-digital.png" 
+              alt="Imagem que ilustra a consultora Dayane Silva"
+              initial={{ y: 60, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.2 }}
+            />
           </figure>
+
           <div className={styles.headlines}>
-            <h1>Você acredita que pode construir uma marca incrível, mas se sente perdida no processo das estratégias?</h1>
-            <h2>Descubra como uma consultoria direcionada para o seu negócio, pode ajudar a sua marca a se destacar!</h2>
+            <motion.div
+              initial={{ x: 60, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.4 }}
+            >
+              <h1>Você acredita que pode construir uma marca incrível, mas se sente perdida no processo das estratégias?</h1>
+              <h2>Descubra como uma <strong>consultoria direcionada para o seu negócio</strong>, pode ajudar a sua marca a se destacar!</h2>
+              <CTA/>
+            </motion.div>
           </div>
         </Container>
         <ShapeDivider/>
